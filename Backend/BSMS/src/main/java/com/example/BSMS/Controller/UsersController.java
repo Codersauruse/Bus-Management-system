0628b9e76d300e50904d3f0fbaca3df8d6e349ab
Codersauruse <shehandesk123@gmail.com>
2024-08-22 21:13:11 +0530

    everything updated

--- Backend/BSMS/src/main/java/com/example/BSMS/Controller/UsersController.java
+++ Backend/BSMS/src/main/java/com/example/BSMS/Controller/UsersController.java
@@ -2,8 +2,12 @@ package com.example.BSMS.Controller;
 
 
 import com.example.BSMS.DTO.Request.UsersRegisterDTO;
+import com.example.BSMS.DTO.Response.UserResponseDTO;
 import com.example.BSMS.Service.UsersService;
+import com.example.BSMS.Util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,18 +19,18 @@ public class UsersController {
     UsersService usersService;
 
     @PostMapping("/register")
-    public String registerUser(@RequestBody UsersRegisterDTO UsersRegisterDTO){
+    public ResponseEntity<StandardResponse> registerUser(@RequestBody UsersRegisterDTO UsersRegisterDTO){
       String message = usersService.registerUser(UsersRegisterDTO);
-      return message;
+      return new ResponseEntity<>(new StandardResponse(200,"sucess",message), HttpStatus.CREATED);
     }
 
     @GetMapping(
             path="/login",
             params = {"username","password"}
     )
-    public String loginUser(@RequestParam (value = "username")String username, @RequestParam(value = "password") String password){
-         String message = usersService.loginUser(username,password);
-         return message;
+    public ResponseEntity<StandardResponse> loginUser(@RequestParam (value = "username")String username, @RequestParam(value = "password") String password){
+         UserResponseDTO userResponseDTO = usersService.loginUser(username,password);
+        return new ResponseEntity<>(new StandardResponse(201,"sucess",userResponseDTO), HttpStatus.OK);
     }
 
 }
